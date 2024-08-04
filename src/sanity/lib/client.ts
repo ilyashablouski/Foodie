39ff8069769963015f20ad6b7233b4cf31d5c700
i.shablovskiy <ilya.shablovskiy@gmail.com>
2024-08-04 18:08:15 +0300

    add sanityFetch query

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,10 +1,27 @@
-import { createClient } from 'next-sanity'
+import { createClient, type QueryParams } from 'next-sanity';
 
-import { apiVersion, dataset, projectId } from '../env'
+import { apiVersion, dataset, projectId } from '../env';
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
   useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
-})
+});
+
+export async function sanityFetch<QueryResponse>({
+  query,
+  params = {},
+  tags,
+}: {
+  query: string;
+  params?: QueryParams;
+  tags?: string[];
+}) {
+  return client.fetch<QueryResponse>(query, params, {
+    next: {
+      revalidate: process.env.NODE_ENV === 'development' ? 30 : 3600,
+      tags,
+    },
+  });
+}
